1556b2912c040d3729e4fad44804b9ad9b1801ee
DePingus <depingus@hotmail.com>
2024-10-02 20:07:44 -0400

    update trash

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("This is a test");
+import { getContext } from "../../extensions.js";
+import { eventSource, event_types } from "../../../../script.js";
+
+// Register an event listener for incoming messages
+eventSource.on(event_types.MESSAGE_RECEIVED, handleIncomingMessage);
+
+// Retrieve application context, including chat logs and participant info
+const context = getContext();
+
+function handleIncomingMessage(data) {
+    // Access the most recent message from the chat log
+    let mostRecentMessage = context.chat[context.chat.length - 1];
+      Console.log(mostRecentMessage.name);
+}
